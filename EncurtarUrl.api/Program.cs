470815dbc9e6899a74c6d469324bfc32046ab0b5
Bruno da Silva Freitas <brunodasilvafreitas@outlook.com>
2025-07-07 23:10:21 -0300

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- EncurtarUrl.api/Program.cs
+++ EncurtarUrl.api/Program.cs
@@ -14,12 +14,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("EncurtarUrlDb"));
 Configuration.BackEndUrl = builder.Configuration.GetValue<string>("BackEndUrl") ?? string.Empty;
+Configuration.FrontEndUrl = builder.Configuration.GetValue<string>("FrontEndUrl") ?? string.Empty;
 
 
 builder.Services.AddTransient<IUrlHandler, Urlhandler>();
 builder.Services.AddTransient<UrlShortenedService>();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x => { x.CustomSchemaIds(type => type.FullName); });
 
 var app = builder.Build();
 
