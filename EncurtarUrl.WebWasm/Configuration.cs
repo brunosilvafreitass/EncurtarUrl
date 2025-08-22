ec7cad300da540073c6b8ffde5f911006f862177
Bruno S. Freitas <bsilva@ramavi.int>
2025-08-22 16:30:25 -0300

    modificando portas

--- EncurtarUrl.WebWasm/Configuration.cs
+++ EncurtarUrl.WebWasm/Configuration.cs
@@ -3,5 +3,5 @@ namespace EncurtarUrl.WebWasm;
 public class Configuration
 {
     public const string HttpClientName = "EncurtarUrl";
-    public static string BackEndUrl { get; set; } = "http://localhost:5048";
+    public static string BackEndUrl { get; set; } = "http://192.168.18.15:5048";
 }
